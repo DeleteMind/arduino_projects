0f126e7c057b9db7f0295dccd247b97ea1f14e06
Filipe Santos Cordeiro <167776782+DeleteMind@users.noreply.github.com>
2025-03-23 17:23:48 +0000

    change_flashingLight

--- Flashing_Light/script.ino
+++ Flashing_Light/script.ino
@@ -1,13 +1,15 @@
+
+int delayTime = 300;
+
 void setup() {
-    // put your setup code here, to run once:
-    pinMode(LED_BUILTIN, OUTPUT);
-  }
-  
-  void loop() {
-    // put your main code here, to run repeatedly:
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(300);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(300);
-  }
-  
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(delayTime);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(delayTime);
+}
